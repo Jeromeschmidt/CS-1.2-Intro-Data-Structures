4a397343c082fc23df3ae71d83eab99bb1623277
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-22 11:57:06 -0700

    added reverse of rearranged phrases

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -7,9 +7,15 @@ def rearrange(test):
         random_word = random.choice(test)
         result += random_word + " "
         test.remove(random_word)
+    result = result [:-1]
     return(result)
 
+def reverse(test):
+    result = test[::-1]
+    return(result)
 
 if __name__ == '__main__':
     test = list(sys.argv[1:])
-    print(rearrange(test))
+    temp = rearrange(test)
+    print(temp)
+    print(reverse(temp))
