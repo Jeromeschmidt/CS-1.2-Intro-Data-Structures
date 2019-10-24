aa35448160561cf0fe97db6aab50cbb042e408fd
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-23 20:13:48 -0700

    added dict histogram without removing punctuation

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,3 +1,4 @@
+import sys
 from string import punctuation
 import re
 
@@ -23,23 +24,11 @@ def frequency(hista, word):
     return False
 
 if __name__ == '__main__':
-    words = ""
-    punctuation = list('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~¢½Â£¨')
-    text = list(open("sherlock.txt",'r').read().strip())
-    dict = create_histogram(text)
-    for punc in punctuation:
-        print(punc)
-        if(punc in dict):
-            while(punc in dict):
-                dict.pop(punc)
 
-    # for line in text.readlines():
-    #     temp = line.readline()
-    # for line in text:
-    #     for word in line:
-    #         words = ''.join([word for word in text if letter not in punctuation])
-    #print("".join(text))
-    print(dict)#create_histogram("".join(text)))
+    with open("sherlock.txt",'r') as file:
+        text = file.read().split()
+
+    dict = create_histogram(text)
+
     print(unique_words(create_histogram(text)))
-    print(frequency(create_histogram(text), "mystery"))
-    # print(text)
+    print(frequency(create_histogram(text), sys.argv[1]))
