4aeb1b0be2330901e4379bb51b4449564f91f6b0
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-27 15:09:23 -0700

    finished list of lists, will clean up code

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,32 +1,82 @@
 import sys
 
-def create_histogram(words):
+def create_dict_histogram(words):
     """returns a histgram of text entered"""
-    hista = {}
+    histo = {}
     for word in words:
-        if word not in hista:
-            hista[word] = 1
+        if word not in histo:
+            histo[word] = 1
         else:
-            hista[word] += 1
-    return hista
+            histo[word] += 1
+    return histo
 
-def unique_words(hista):
-    """returns number of funique words"""
-    total_words = len(hista)
+def create_list_histogram(words):
+    """create list of lists histogram"""
+    histo = list()
+    for word in words:
+        num = 0
+        word = word.lower()
+        for entry in words:
+            entry = entry.lower()
+            if word == entry:
+                num += 1
+        if (word, num) not in histo:
+            histo.append((word, num))
+    return histo
+
+def create_l_of_t_histogram(words):
+    """create list of tuples histogram"""
+    histo = list()
+    for i in range(len(words)):
+        if (words[i], 1) in histo:
+            print("HEREHEREHERE")
+            for j in range(len(histo)):
+                if(histo[j][0] == words[i]):
+                    word = histo[j][0]
+                    num = histo[j][1]
+                    histo.remove(histo[j])
+                    histo.append((word, num+1))
+        else:
+            histo.append((words[i], 1))
+    return histo
+
+
+def unique_words(histo):
+    """returns number of unique words"""
+    total_words = len(histo)
     return total_words
 
-def frequency(hista, word):
+def unique_words_list(histo):
+    count = {}
+    for word in histo:
+        if word not in count:
+            count[word] = 1
+    return len(count)
+
+def frequency_in_dict(histo, word):
     """returns frequency of a certain word in text"""
-    if(word in hista):
-        return hista[word]
+    if(word in histo):
+        return histo[word]
+    return False
+
+def frequency_in_list(histo, word):
+    """returns frequency of a certain word in text"""
+    for i in range(len(histo)):
+        # print(histo[i])
+        if(histo[i][0] == word):
+            return histo[i][1]
     return False
 
 if __name__ == '__main__':
 
-    with open("sherlock.txt",'r') as file:
+    with open("short_text.txt",'r') as file:
         text = file.read().split()
+    file.close()
 
-    dict = create_histogram(text)
+    # dict = create_dict_histogram(text)
+    l_of_l = create_list_histogram(text)
+    # l_of_t = create_l_of_t_histogram(text)
 
-    print(unique_words(create_histogram(text)))
-    print(frequency(create_histogram(text), sys.argv[1]))
+    print(l_of_l)
+    print(unique_words_list(l_of_l))
+    print(frequency_in_list(l_of_l, sys.argv[1]))
