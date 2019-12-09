f9a296f1eec6dcc06bb8b0988f0054d8215aa313
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-12-09 09:51:55 -0800

    implemented queue class

---
+++ Code/queue.py
@@ -0,0 +1,31 @@
+
+class Queue(list):
+
+    def __init__(self):
+        """Initialize this node with the given data."""
+        self = list()
+
+    def enqueue(self, item):
+        self.append(item)
+
+    def dequeue(self):
+        return self.pop(0)
+
+    def iterate(self):
+        all_items = []
+        for item in self:
+            all_items.append(item)
+        return all_items
+
+def test_queue():
+    q = Queue()
+    q.enqueue((1, 1))
+    q.enqueue((2, 2))
+    q.enqueue((3, 3))
+    print(q.iterate())
+    print(q.dequeue())
+    print(q.dequeue())
+    print(q.dequeue())
+
+if __name__ == '__main__':
+    test_queue()
--- Code/tweet-gen/markov_chain_nth_order.py
+++ Code/tweet-gen/markov_chain_nth_order.py
@@ -4,8 +4,9 @@ from dictogram import Dictogram
 import string
 
 class MarkovChain(Dictogram):
-    def __init__(self, word_list):
+    def __init__(self, word_list, order):
         super().__init__()
+        self.order = order
         self.start_tokens = Dictogram()
         self.stop_tokens = Dictogram()
 
@@ -22,52 +23,48 @@ class MarkovChain(Dictogram):
                 word_list[i] = re.sub("[^a-zA-Z]", '', word_list[i])
                 # word_list[i] = word_list[i][:len(word_list[i])-1]
                 self.stop_tokens.add_count(word_list[i], 1)
-        for i in range(len(word_list)-1):
-            word_list[i] = re.sub("[^a-zA-Z]", '', word_list[i])
-            word_list[i+1] = re.sub("[^a-zA-Z]", '', word_list[i+1])
-            if word_list[i] in self:
-                self[word_list[i].lower()].add_count(word_list[i+1].lower(), 1)
+        for i in range(len(word_list)-self.order):
+            temp = list()
+            for j in range(self.order):
+                word_list[i+j] = re.sub("[^a-zA-Z]", '', word_list[i+j])
+                temp.append(word_list[i+j].lower())
+            temp = tuple(temp)
+            if temp in self:
+                self[temp].add_count(word_list[i+self.order].lower(), 1)
             else:
-                self[word_list[i].lower()] = Dictogram([word_list[i+1].lower()])
+                self[temp] = Dictogram([word_list[i+self.order].lower()])
 
     def random_walk(self, length=10):
         sentence = ""
         keys = list(self.keys())
-        word = self.start_word()
+        word = self.start_words()
         sentence += word + " "
         word = word.lower()
         for i in range(length-1):
             word = self[word].sample()
             sentence += word + " "
-        sentence = sentence + self.end_word() + ". "
+        sentence = sentence + self.end_word() + "."
         return sentence
 
-    def start_word(self):
-        ###for 1st order markov chain
-        dart = random.randint(0, len(self.start_tokens)+1)
-        fence = 0
-        for elm in self.start_tokens:
-            for key in self.start_tokens.keys():
-                fence += self.start_tokens[key]
-                if fence > dart:
-                    return elm.capitalize()
-
+    def start_words(self):
+        keys = list(self.keys())
+        words = random.choice(keys)
+        if words[0] in self.stop_tokens:
+            start_sentence = ""
+            for i in range(len(words)):
+                start_sentence += words[i] + " "
+            return start_sentence
+        else:
+            self.start_words()
 
     def end_word(self):
-        dart = random.randint(0, len(self.stop_tokens)+1)
-        fence = 0
-        while 1:
-            for elm in self.stop_tokens:
-                for key in self.stop_tokens.keys():
-                    fence += self.stop_tokens[key]
-                    if fence >= dart:
-                        return elm
+        pass
 
 if __name__ == '__main__':
-    words = "One fish blue fish. Red fish blue fish."
+    words = "Blue fish blue fish. Blue fish blue fish."
     word_list = words.split()
     print(word_list)
     # word_list = ["Blue", "One.", "fish.", "One", "two","blue", "fish", "two", "red", "fish", "blue", "red", "blue", "fish", "red", "blue", "fish"]
-    markovChain = MarkovChain(word_list)
+    markovChain = MarkovChain(word_list, 3)
     print(markovChain)
     print(markovChain.random_walk(20))
